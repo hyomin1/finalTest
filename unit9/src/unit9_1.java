9690bf3cee6a681cb5adfd3b1d29a906b1e3d8bd
leehm123456 <leehm123456@gmail.com>
2022-12-07 00:25:33 +0900

    명품 자바 9-1

---
+++ unit9/src/unit9_1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+class prac1 extends JFrame {
+	prac1() {
+		super("Let's study Java");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400,200);
+		setVisible(true);
+	}
+}
+public class unit9_1 {
+
+	public static void main(String[] args) {
+		new prac1();
+
+	}
+
+}
